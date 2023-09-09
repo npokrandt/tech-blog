14cf7a959bcf7fa436fa83fb2de6b613e93aaac3
npokrandt <npokrandt919@gmail.com>
2023-09-09 01:18:08 -0500

    designing models, and adds sequelize connection file

--- models/index.js
+++ models/index.js
@@ -2,5 +2,16 @@
 
 /*
 User (duh)
-
+    username
+    password (encrypted)
+Blogpost
+    title
+    content
+    comment_number
+    user_id (connects to user)
+Comment
+    username
+    content
+    blogpost_id
+    user_id
 */
