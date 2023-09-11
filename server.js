1d0bad4c28fb62ca9497d4d80dd7bb8dc3b55d1b
npokrandt <npokrandt919@gmail.com>
2023-09-11 12:13:22 -0500

    adds main handlebars files, and controller route to see the homepage, along with some bug fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
-//const routes = require('./controllers')
+const routes = require('./controllers')
 //use if necessary
 //const helpers = require('./utils')
 
@@ -33,9 +33,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-//app.use(routes)
+app.use(routes)
 
-sequelize.sync({force: false}, () => {
+sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
         console.log(`app listening on port ${PORT}`)
     })
