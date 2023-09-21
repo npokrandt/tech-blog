3d807117fe292fb2f379053265fa615792e7394e
npokrandt <npokrandt919@gmail.com>
2023-09-21 17:53:50 -0500

    adds the login and create account views and styles them

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -3,6 +3,7 @@ const { Blogpost } = require('../models');
 const router = require('express').Router()
 // const {User} = require('../models')
 
+//homepage
 router.get('/', async (req, res) => {
 
     const blogposts = await Blogpost.findAll({raw: true})
@@ -12,6 +13,31 @@ router.get('/', async (req, res) => {
         res.render('homepage', {
             blogposts
         })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//login
+router.get('/login', async (req, res) => {
+
+    //const blogposts = await Blogpost.findAll({raw: true})
+
+    try {
+ 
+        res.render('login')
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/create-account', async (req, res) => {
+
+    //const blogposts = await Blogpost.findAll({raw: true})
+
+    try {
+ 
+        res.render('create-account')
     } catch (err) {
         res.status(500).json(err);
     }
