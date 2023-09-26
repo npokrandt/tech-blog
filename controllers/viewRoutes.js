5ead257294579c364ff1a2c539fab1858f190430
npokrandt <npokrandt919@gmail.com>
2023-09-26 10:53:21 -0500

    adds dashboard page design

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -32,6 +32,35 @@ router.get('/', async (req, res) => {
     }
 })
 
+//dashboard
+router.get('/dashboard', async (req, res) => {
+
+    const blogpostData = await Blogpost.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        include: User,
+    })
+
+    const blogposts = blogpostData.map(blogpost => blogpost.get({ plain: true }))
+    //console.log(blogposts)
+
+    for (blogpost in blogposts){
+        const post = blogposts[blogpost]
+        const newDate = new dayjs(post.updatedAt).format('MMMM DD, YYYY')
+        post.formatted_date = newDate
+    }
+
+    try {
+        res.render('dashboard', {
+            blogposts,
+            logged_in: req.session.logged_in,
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 //login
 router.get('/login', async (req, res) => {
 
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const hbs = exphbs.create()
 const sess = {
     secret: process.env.SECRET,
     cookie: {
-        maxAge: 300000
+        maxAge: 3600000
     },
     resave: false,
     saveUninitialized: true,
